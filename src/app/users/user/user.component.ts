6ed0c30ef9120ac22feb67ee2e2cc7404442bf67
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-10 03:16:14 +0530

    unsubscribing using destroy hook

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-user",
   templateUrl: "./user.component.html",
   styleUrls: ["./user.component.css"],
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: { id: number; name: string };
 
+  paramsSubscription: Subscription;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
@@ -16,9 +18,15 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params["id"],
       name: this.route.snapshot.params["name"],
     };
-    this.route.params.subscribe((updatedParams: Params) => {
-      this.user.id = updatedParams["id"];
-      this.user.name = updatedParams["name"];
-    });
+    this.paramsSubscription = this.route.params.subscribe(
+      (updatedParams: Params) => {
+        this.user.id = updatedParams["id"];
+        this.user.name = updatedParams["name"];
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe();
   }
 }
